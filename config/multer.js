062ac27aa2d28f2bf09edb71c9f7e115c78fb812
sshreyasingh <captainshreya007@gmail.com>
2025-08-22 01:52:12 +0530

    new changes

--- config/multer.js
+++ config/multer.js
@@ -1,11 +1,4 @@
-// const multer = require('multer');
 
-// // Use memory storage to get access to file.buffer (required for Supabase upload)
-// const upload = multer({ storage: multer.memoryStorage() });
-
-// module.exports = upload;
-
-// config/multer.js
 const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
