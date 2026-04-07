dcb51a541e241500fff68035a1088456c6e2fa6b
sshreyasingh <captainshreya007@gmail.com>
2026-04-07 17:54:27 +0530

    changed server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
+// const app = require("./app");
+
+// const PORT = process.env.PORT || 3001;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+// module.exports = app;
 const app = require("./app");
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 module.exports = app;
